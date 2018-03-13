2735e157b70e9820faab2519f6eeebe42e7e1f8d
Pawich <pawich@Pawichs-MacBook-Pro.local>
2018-03-14 01:12:13 +0700

    Initial Commit

---
+++ NotificX/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  NotificX
+//
+//  Created by Pawich on 3/14/2561 BE.
+//  Copyright © 2561 pawichhi. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
